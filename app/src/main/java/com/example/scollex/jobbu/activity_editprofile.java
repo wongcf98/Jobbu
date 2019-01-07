d04b7d775d8b904b0907829ee4681e00f6442ba9
wongcf98 <44184737+wongcf98@users.noreply.github.com>
2019-01-08 00:09:01 +0800

    register will direct to mainactivity login with firebase realtim database

--- app/src/main/java/com/example/scollex/jobbu/activity_editprofile.java
+++ app/src/main/java/com/example/scollex/jobbu/activity_editprofile.java
@@ -10,14 +10,50 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class activity_editprofile extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class activity_editprofile extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    private Button editBtn, cancelBtn;
+    private Spinner gender,level,jobType;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRegerence;
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
 
+        initializeSpinner();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRegerence = mDatabase.getReference();
+
+        user = new User();
+        user.setUserID(mUser.getUid());
+        mRegerence.child("User");
+
+
+        editBtn = findViewById(R.id.editprofile_btnEdit);
+        cancelBtn = findViewById(R.id.editprofile_btnCancel);
+        gender = findViewById(R.id.spinnerGender);
+        level = findViewById(R.id.spinnerLevel);
+        jobType = findViewById(R.id.spinnerJobType);
+
+        editBtn.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
+    }
+
+    private void initializeSpinner() {
         Spinner spinner = findViewById(R.id.spinnerGender);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Gender,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -35,17 +71,41 @@ public class activity_editprofile extends AppCompatActivity implements AdapterVi
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
         spinner2.setOnItemSelectedListener(this);
-
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+        if(parent == gender){
+            String text = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(),"gender: " + text, Toast.LENGTH_SHORT).show();
+        }
+        if(parent == level){
+            String text = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(),"level: " + text, Toast.LENGTH_SHORT).show();
+        }
+        if(parent == jobType){
+            String text = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(),"Job Type: " + text, Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(parent.getContext(),"gender: not selected ", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onClick(View v) {
+        if(v == cancelBtn){
+            finish();
+            startActivity(new Intent(this,MainActivity.class));
+        }
+        if(v == editBtn){
+            saveIntoFireBase();
+        }
+    }
+
+    private void saveIntoFireBase() {
     }
 }
